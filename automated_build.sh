8723bc1964824650297776e30a34dce3776db9a7
Jamey Greenwood <jamey.greenwood@theunit.co.uk>
2017-01-06 11:55:32 +0000

    Modify CircleCI script so the deployment aspect is split by environment.

--- automated_build.sh
+++ automated_build.sh
@@ -1,23 +1,5 @@
-# Copy static site
-CWD=`pwd`
-
-# Clone Pages repository
-cd /tmp
-git clone git@github.com:JameyG-Unit/headless-test-deployed.git build
-
-# Trigger Jekyll rebuild
-cd $CWD
+# Download all content from Contentful
 bundle exec jekyll contentful
+
+# Build the site using Jekyll
 bundle exec jekyll build
-
-# Push newly built repository
-cp -r $CWD/_site/* /tmp/build
-
-cd /tmp/build
-
-git config --global user.email "jamey.greenwood@theunit.co.uk"
-git config --global user.name "Jamey Greenwood"
-
-git add .
-git commit -m "Automated Rebuild"
-git push -f origin master
