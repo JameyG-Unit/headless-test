42e7826359c045d95f8e9270539dc8da94a1de5c
Jamey Greenwood <jamey.greenwood@theunit.co.uk>
2017-01-06 15:22:22 +0000

    Switch over to environment-based base URLs for the Jekyll site build

--- automated_build.sh
+++ automated_build.sh
@@ -2,7 +2,7 @@
 bundle exec jekyll contentful
 
 # Build the site using Jekyll
-bundle exec jekyll build
+bundle exec jekyll build --baseurl /$CIRCLE_PROJECT_REPONAME-$CIRCLE_BRANCH
 
 # Set permissions for the build scripts
 chmod +x deploy_*.sh
